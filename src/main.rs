caabd8892d6fb278c332a9636d95ede02110ddd0
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2022-07-06 14:48:39 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ struct Bar {
     z: u32,
 }
 
+struct Unit;
+
 fn main() {
     let a = minicbor::to_vec(Foo { x: 50, y: 100 }).unwrap();
     let b: Foo = minicbor::decode(&a).unwrap();
@@ -28,4 +30,9 @@ fn foo() {
     let b: Foo = minicbor::decode(&a).unwrap();
 
     assert_eq!(b, Foo { x: 51, y: 101 });
+}
+
+#[test]
+fn unit() {
+    let _ = Unit;
 }
